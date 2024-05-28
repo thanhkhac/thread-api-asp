2ca2e390f57cac2c2e47ad4b7bc25abb6023684d
Khac Thanh <thanhcqb2048@gmail.com>
2024-05-28 10:02:13 +0700

    Add base project

---
+++ Base/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using Microsoft.OpenApi.Models;
+
+namespace Base.Configurations
+{
+    public static class ServiceConfiguration
+    {
+        public static void MyConfigureService(this IServiceCollection services)
+        {
+
+            
+        }
+
+        public static void MyConfigureSwaggerAuthentication(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(option =>
+            {
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+        }
+
+
+    }
+}
